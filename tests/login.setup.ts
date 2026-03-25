9a94f1345bacc37a985639b48a8cf3a0eec23dcf
Siddharth Khatsuriya <siddharth.khatsuriya@crestdata.ai>
2026-03-25 19:50:26 +0530

    chore : Added state storage mechanism to use for authorization

--- tests/login.setup.ts
+++ tests/login.setup.ts
@@ -1,6 +1,10 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../src/page/loginpage';
 import { standardUser } from '../src/auth/user';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
+const statefile = path.resolve(__dirname, '../.auth/state.json');
 
 test('Login Page', async ({ page }) => {
     const loginPage = new LoginPage(page);
@@ -8,4 +12,8 @@ test('Login Page', async ({ page }) => {
     await loginPage.login(standardUser.username, standardUser.password);
     await expect(page).toHaveTitle('Swag Labs'); 
     await expect(page).toHaveURL(/\/inventory\.html$/); // Checking login was successful
+
+    await fs.mkdir(path.dirname(statefile), { recursive: true });
+    await page.context().storageState({ path: statefile }); // Saves the browser context to state file 
+
 });
